c65e061ca820d803dc3bc6f7dc6ce87452e8c367
Redtius <redtius@gmail.com>
2024-01-01 15:23:24 +0100

    added landing page

--- client/src/app/components/landingpage/landingpage.component.ts
+++ client/src/app/components/landingpage/landingpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
